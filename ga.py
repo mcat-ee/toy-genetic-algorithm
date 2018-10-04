ba5460b009ea4b5947dd4fe4a2a62a5245dd9a6b
mcat-ee <tom@mcatee.io>
2018-10-04 11:02:35 +0930

    Adding test code used to display scoring results

--- ga.py
+++ ga.py
@@ -1,9 +1,19 @@
 import sys
 from arguments import arguments
 
+import Strategies.ordered
 import file_manager
+
+import pprint
 
 if __name__ == "__main__":
     config_obj = arguments(sys.argv)
     messages = file_manager.load_messages_from_file(config_obj['message_file'])
-    print(messages)
+    strategies = file_manager.import_libraries()
+    pp = pprint.PrettyPrinter(depth=6)
+
+    test_solutions = strategies['ordered']['generate'](messages['message_list'],3)
+    for solution in test_solutions:
+        pp.pprint(solution)
+        print("SCORE: " + str(strategies['ordered']['objective_function'](solution)))
+        print()
