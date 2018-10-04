a0675b7e34093bb0f1ad26444bb97c440589aea3
mcat-ee <tom@mcatee.io>
2018-10-04 11:46:45 +0930

    Added new generation function to 'ordered' strategy for generating new solutions from existing best solution

--- Strategies/ordered.py
+++ Strategies/ordered.py
@@ -1,14 +1,33 @@
 from strategy import Strategy
 from random import shuffle
-
+import random
 name = "ordered"
 def objective_function(genome):
     score = 0
-    for messageIndex, message in enumerate(genome['ordered_message_list']):
-        if str(messageIndex) == message['id']:
-            score = score + 1
-
+    #print(genome)
+    try:
+        for messageIndex, message in enumerate(genome['ordered_message_list']):
+            if str(messageIndex) == message['id']:
+                score = score + 1
+    except TypeError:
+        score = 0
+        for messageIndex, message in enumerate(genome):
+            if str(messageIndex) == message['id']:
+                score = score + 1
     return score
+
+def generate_from_best(best_solution, population_size):
+    #print(best_solution)
+    population = []     #Population is created with the original best solution as it's founding member
+    population.append({"id": random.random(), "ordered_message_list": best_solution['solution_object']['ordered_message_list'][:]})
+    while(len(population) < population_size):
+        new_solution = best_solution['solution_object']['ordered_message_list'][:]
+        solution_length = range(len(new_solution))
+        index1, index2 = random.sample(solution_length, 2)
+        new_solution[index1], new_solution[index2] = new_solution[index2], new_solution[index1]
+        population.append({"id": random.random(), "ordered_message_list": new_solution})
+    return population
+
 
 def generate(message_list, population_size):
     solutions = []
