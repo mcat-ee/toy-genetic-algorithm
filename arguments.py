8eae8cfcaf4df10692e7f67721c89859994e8218
mcat-ee <tom@mcatee.io>
2018-10-04 10:13:19 +0930

    Removed 'help' and 'test' values from command line arg count evaluation

--- arguments.py
+++ arguments.py
@@ -1,18 +1,39 @@
 import json
 
-def arguments(argv):
-    not_enough_arguments_flag = len(argv)< 3
-    if 'help' in argv or not_enough_arguments_flag:
-        if not_enough_arguments_flag: #TODO: Remove 'help' and 'test' from argv length check
-            print("Not enough arguments provided")
-        print_help_text()
-        exit(0)
 
+def arguments(argv):
+    '''
+        Expects arguments of the form:
+            ga.py message_file_location fitness_function_name [test help]
+    '''
     config = {}
+
     if 'test' in argv:
         config['test'] = True
     else:
         config['test'] = False
+    try:
+        argv.remove('test')
+    except ValueError:
+        pass
+
+    if 'help' in argv:
+        config['help'] = True
+    else:
+        config['help'] = False
+    try:
+        argv.remove('help')
+    except ValueError:
+        pass
+
+    not_enough_arguments_flag = len(argv)< 3
+    if config['help'] or not_enough_arguments_flag:
+        if not_enough_arguments_flag:
+            print("Not enough arguments provided")
+        print_help_text()
+        exit(0)
+
+
 
     config['message_file'] = argv[1]
     print("Message file location: '" + config['message_file'] +"'" )
