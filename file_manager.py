f4eb7bfc954640e344aa250216524b1f9584cd11
mcat-ee <tom@mcatee.io>
2018-10-04 11:01:30 +0930

    Added code for loading strategy files (Very manual at the moment)

--- file_manager.py
+++ file_manager.py
@@ -1,7 +1,13 @@
 import json
+from Strategies import ordered
 
 def load_messages_from_file(file_location):
     messages = None
     with open(file_location) as f:
         messages = json.load(f)
     return messages
+
+def import_libraries():
+    strategies = {}
+    strategies[ordered.name] = { "objective_function": ordered.objective_function, "generate": ordered.generate}
+    return strategies
